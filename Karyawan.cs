29027fd07618568206f4cd4b348029e6c007e37d
Yakobus Erpindi Purwanto <erpindi.cc@gmail.com>
2020-04-15 12:05:18 +0700

    Karyawan.cs

---
+++ Karyawan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pindi
+{
+    class Karyawan
+    {
+       
+        
+            public int No { get; set; }
+            public int Nik { get; set; }
+            public string Nama { get; set; }
+            public int Gaji { get; set; }
+
+
+
+            public void InfoKaryawan()
+            {
+                if (Gaji < 0)
+                {
+                    Gaji = 0;
+                }else
+            {
+                Gaji = Gaji;
+            }
+
+                Console.WriteLine("{0}   {1}\t{2}\t{3}        ", No, Nik, Nama, Gaji);
+
+            }
+
+            public void BonusGaji()
+            {
+               
+            double tmp = 0;
+            tmp = 0.1 * Gaji;
+            Gaji += Convert.ToInt32(tmp);
+            Console.WriteLine("{0}  {1}\t{2}\t{3}        ", No, Nik, Nama, Gaji * 0.1 + Gaji);
+
+            }
+
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace pindi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama : Yakobus Erpindi Purwanto");
+            Console.WriteLine("NIM  : 19.11.2835");
+            Console.WriteLine("");
+
+            Karyawan karyawan1 = new Karyawan();
+            Karyawan karyawan2 = new Karyawan();
+            
+            karyawan1.No = 1;
+            karyawan1.Nik = 190302123;
+            karyawan1.Nama = "Paijo";
+            karyawan1.Gaji = 3000000;
+
+            karyawan2.No = 2;
+            karyawan2.Nik = 190302124;
+            karyawan2.Nama = "Jono";
+            karyawan2.Gaji = 2000000;
+
+           
+
+            Console.WriteLine("No    NIK/Nama \t\tGaji     ");
+            Console.WriteLine("----------------------------------------");
+            karyawan1.InfoKaryawan();
+            karyawan2.InfoKaryawan();
+           
+            Console.WriteLine("");
+            Console.WriteLine("Asyikk dapat kenaikan gaji 10%");
+            Console.WriteLine("");
+            Console.WriteLine("No    NIK/Nama \t\tGaji     ");
+            Console.WriteLine("----------------------------------------");
+            karyawan1.BonusGaji();
+            karyawan2.BonusGaji();
+            
+
+            Console.ReadKey();
+        }
+    }
+    
+}
